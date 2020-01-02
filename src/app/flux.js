50185b1af6a10cc6ba6568647074162a8c0b2f18
Hữu Ðà Trần <huuda.tran@quoininc.com>
2020-01-02 16:46:04 -0500

    - #8: Allow complete a task

--- src/app/flux.js
+++ src/app/flux.js
@@ -13,17 +13,20 @@ import namespace from './namespace';
 
 const actions = namespacedActions(namespace, [
     'ADD',
+    'COMPLETE',
     'INIT_TYPE',
     'REMOVE'
 ]);
 
 const actionCreators = Object.freeze({
     add: (value) => actionCreator(actions.ADD, { value }),
+    complete: (index) => actionCreator(actions.COMPLETE, { index }),
     remove: (index) => actionCreator(actions.REMOVE, { index })
 });
 
 export const orchestrators = Object.freeze({
     add: (dispatch, value) => dispatch(actionCreators.add(value)),
+    complete: (dispatch, index) => dispatch(actionCreators.complete(index)),
     remove: (dispatch, index) => dispatch(actionCreators.remove(index))
 });
 
@@ -38,6 +41,15 @@ export const reducers = concatenateReducers([{
             value: action.payload.value,
             created: Date.now()
         }]));
+        return setSubstateAttribute(state, namespace, ATTRIBUTES.TASKS, newTasks);
+    }
+}, {
+    actions: [ actions.COMPLETE ],
+    reducer: (state, action) => {
+        const tasks = getSubstateAttribute(state, namespace, ATTRIBUTES.TASKS, fromJS([]));
+        const task = tasks.get(action.payload.index, fromJS({}));
+        const newTask = task.set('completed', Date.now());
+        const newTasks = tasks.set(action.payload.index, newTask);
         return setSubstateAttribute(state, namespace, ATTRIBUTES.TASKS, newTasks);
     }
 }, {
--- src/tasks/component.jsx
+++ src/tasks/component.jsx
@@ -8,7 +8,15 @@ import Task from './../task';
 import { NAME } from './constants';
 
 const Component = (props) => {
-    const tasks = props.tasks.map((task, index) => <Task key={index} task={task} />);
+    const tasks = props.tasks.map((task, index) => {
+        if (task.deleted) {
+            return null;
+        } else if (task.completed) {
+            return null;
+        } else {
+            return <Task key={index} task={task} />;
+        }
+    });
 
     return (
         <div className="quoin-react-utils-todo-tasks">
